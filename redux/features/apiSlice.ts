a608e0ff49ddc4bad193a501e355dcaf9057fd76
Arvin <rvin.ramezani@gmail.com>
2023-03-05 10:14:16 +0330

    Work on apiSlice baseUrl

--- redux/features/apiSlice.ts
+++ redux/features/apiSlice.ts
@@ -15,7 +15,7 @@ import transformImageUrl from '@/utils/common/transform-image-url';
 const baseUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000/api'
-    : `https://${process.env.VERCEL_URL}/api`;
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
 
 export const foodsApi = createApi({
   reducerPath: 'foodsApi',
