dfb8ac81c30b37e3a3ce7517586e90202d027ec3
Arvin <rvin.ramezani@gmail.com>
2023-03-08 17:35:18 +0330

    Use formidable for extract /auth/me route formData

--- pages/api/auth/me-handlers.ts
+++ pages/api/auth/me-handlers.ts
@@ -1,3 +1,4 @@
+import formidable from 'formidable';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
 import fs from 'fs';
@@ -57,45 +58,93 @@ export async function usersPatchHandler(
   res: NextApiResponse
 ) {
   try {
-    // await runMulterMiddleware(req, res, upload.single('image'));
+    if (process.env.NODE_ENV !== 'production') {
+      await runMulterMiddleware(req, res, upload.single('image'));
 
-    // const secret = process.env.JWT_SECRET;
-    // const session = await getToken({ req, secret });
-    // if (!session) {
-    //   res.status(401).json({ message: 'You are not authenticated' });
-    //   return;
-    // }
+      const secret = process.env.JWT_SECRET;
+      const session = await getToken({ req, secret });
+      if (!session) {
+        res.status(401).json({ message: 'You are not authenticated' });
+        return;
+      }
 
-    // await dbConnect();
-    // const user = await User.findById(session?.sub);
+      await dbConnect();
+      const user = await User.findById(session?.sub);
 
-    // if (!user) {
-    //   res.status(404).json({ message: 'User not found' });
-    //   return;
-    // }
+      if (!user) {
+        res.status(404).json({ message: 'User not found' });
+        return;
+      }
 
-    // const isValid = await patchBodySchema.isValid(req.body);
-    // if (!isValid) {
-    //   return res.status(422).json({ message: 'Invalid inputs' });
-    // }
+      const isValid = await patchBodySchema.isValid(req.body);
+      if (!isValid) {
+        return res.status(422).json({ message: 'Invalid inputs' });
+      }
 
-    // user.firstName = req.body.firstName;
-    // user.lastName = req.body.lastName;
-    // user.email = req.body.email;
-    // user.phone = req.body.phone;
+      user.firstName = req.body.firstName;
+      user.lastName = req.body.lastName;
+      user.email = req.body.email;
+      user.phone = req.body.phone;
 
-    // if (req.file) {
-    //   user.image && fs.unlink(user.image, (err) => console.log(err));
+      if (req.file) {
+        user.image && fs.unlink(user.image, (err) => console.log(err));
 
-    //   user.image = req.file.path;
-    // } else if (req.body.image === 'DELETE') {
-    //   user.image && fs.unlink(user.image, (err) => console.log(err));
-    //   user.image = undefined;
-    // }
+        user.image = req.file.path;
+      } else if (req.body.image === 'DELETE') {
+        user.image && fs.unlink(user.image, (err) => console.log(err));
+        user.image = undefined;
+      }
 
-    // await user.save();
-    // res.status(200).json(user);
-    return res.status(200);
+      await user.save();
+      res.status(200).json(user);
+      return;
+    }
+
+    const secret = process.env.JWT_SECRET;
+    const session = await getToken({ req, secret });
+    if (!session) {
+      res.status(401).json({ message: 'You are not authenticated' });
+      return;
+    }
+
+    await dbConnect();
+    const user = await User.findById(session?.sub);
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    const form = formidable({ multiples: true });
+
+    const formData = new Promise<{
+      fields: formidable.Fields;
+      files: formidable.Files;
+    }>((resolve, reject) => {
+      form.parse(req, async (err, fields, files) => {
+        if (err) {
+          reject('error');
+        }
+        resolve({ fields, files });
+      });
+    });
+
+    const { fields, files } = await formData;
+    console.log(files, '/auth/me | PATCH | (files)');
+
+    const isValid = await patchBodySchema.isValid(fields);
+    if (!isValid) {
+      return res.status(422).json({ message: 'Invalid inputs' });
+    }
+
+    user.firstName = fields.firstName as string;
+    user.lastName = fields.lastName as string;
+    user.email = fields.email as string;
+    user.phone = fields.phone as string;
+
+    await user.save();
+    res.status(200).json(user);
+    return;
   } catch (error) {
     res.status(500).json({ message: 'Somethin went wrong !' });
   }
